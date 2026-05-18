b98d13a2cfe930518d638f3c223455713fb7e227
Dan Ryan <scriptfu@gmail.com>
2026-05-18 14:13:40 -0400

    shim(OC_DAC): add OC::DAC::kOctaveZero constant for MIDIQuantizer

--- shim/include/OC_DAC.h
+++ shim/include/OC_DAC.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 
 enum DAC_CHANNEL {
     DAC_CHANNEL_A,
@@ -7,3 +8,12 @@ enum DAC_CHANNEL {
     DAC_CHANNEL_D,
     DAC_CHANNEL_LAST
 };
+
+namespace OC {
+namespace DAC {
+// Octave bias for MIDIQuantizer note-number conversion. Vendor
+// OC_DAC.h defines this on the target hardware as 5; tests and
+// vendor MIDIQuantizer pass it as the default `bias` parameter.
+static constexpr uint8_t kOctaveZero = 5;
+}
+}
